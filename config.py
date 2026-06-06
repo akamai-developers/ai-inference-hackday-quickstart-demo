407f32eb532b8febca1dd608cf5d12baf3d0155b
Sheilah <kirui.sheilah@gmail.com>
2026-06-06 15:41:50 -0700

    clean --replace new files

--- config.py
+++
@@ -1,9 +0,0 @@
-# The underlying physical endpoints listening on your Akamai Cloud VM
-LARGE_MODEL_URL = "http://172.232.238.91:8000/v1/chat/completions"
-SMALL_MODEL_URL = "http://172.232.238.91:8001/v1/chat/completions"
-
-# Model identifier definitions used to build the metadata trace
-LARGE_MODEL = "meta-llama/Meta-Llama-3-70B-Instruct"
-SMALL_MODEL = "meta-llama/Meta-Llama-3-8B-Instruct-AWQ"
-
-DEFAULT_TIMEOUT_SECONDS = 30
